ba03270e85f7ee893e900b90a997d7f593ef5737
Arnav Kohli <arnavkohli2003@gmail.com>
2024-02-14 02:09:58 +0000

    Change wsgi
    Changed wsgi to run the command before main is called.
    
    Signed-off-by: Arnav Kohli <arnavkohli2003@gmail.com>

--- api/ip.py
+++ api/ip.py
@@ -5,6 +5,7 @@ def push_public_ip():
     try:
         response = requests.get('https://api.ipify.org')
         if response.status_code == 200:
+            print('Success!!!')
             db.child("IP").set(response.text)
         else:
             print("Error: Unable to get IP address")
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from api import app, ip
 
+ip.push_public_ip()
+
 if __name__ == "__main__":
-    ip.push_public_ip()
     app.run(debug=True, host="0.0.0.0", port=8080)
