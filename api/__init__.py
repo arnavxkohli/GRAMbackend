19cecc74112f7abbaa415d58ac8bebb18551466a
Arnav Kohli <arnavkohli2003@gmail.com>
2024-02-14 03:33:12 +0000

    Fix circular imports
    Signed-off-by: Arnav Kohli <arnavkohli2003@gmail.com>

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 import pyrebase
+import requests
 
 firebase_client_config = {
     "apiKey": "AIzaSyB5oYH_7CMXhDwPdMudUlhpNWrjmADxX6o",
@@ -20,18 +21,31 @@ auth = pb.auth()
 
 db = pb.database()
 
+def push_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org')
+        if response.status_code == 200:
+            print('Success!!!')
+            db.child("IP").set(response.text)
+        else:
+            print("Error: Unable to get IP address")
+    except Exception as e:
+        return f"Error: {e}"
+
+
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.secret_key = "ea9873ab493d0c43d24d89ee1f96080b91521d3c6ae0e0199a673ffef92e2021" #GRAM sha256 hash
 
     with app.app_context():
-        ip.push_public_ip()
+        push_public_ip()
 
     return app
 
+app = create_app()
 
-from api import endpoints, ip
+from api import endpoints
 
 
 
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-from api import create_app
+from api import app
 
 
 if __name__ == "__main__":
     # Run the app in production mode
-    create_app().run(debug=False, host="0.0.0.0", port=8080)
+    app.run(debug=False, host="0.0.0.0", port=8080)
